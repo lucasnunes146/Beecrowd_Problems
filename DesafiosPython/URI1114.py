5edc56b208f13a236ccc74856aaa3b10d55e7a3a
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 11:47:38 -0300

    Senha Fixa

---
+++ DesafiosPython/URI1114.py
@@ -0,0 +1,8 @@
+senha = 0
+while senha != 2002:
+    senha = int(input())
+    if senha == 2002:
+        print('Acesso Permitido')
+        break
+    if senha != 2002:
+        print('Senha Invalida')
