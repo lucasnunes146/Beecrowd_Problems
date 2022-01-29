24ef51c2a4f68b3f2d5535534069c2031a7195b9
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 10:18:02 -0300

    Gasto de Combustível

---
+++ DesafiosJava/URI1017.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class URI1017 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int horas = entrada.nextInt();
+        int velocidade = entrada.nextInt();
+        double calculo = horas * velocidade;
+        double litros = calculo/12;
+        System.out.printf("%.3f\n", litros);
+        entrada.close();
+    }
+}
