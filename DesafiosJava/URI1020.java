5f7fa351c4d4daf67a5d331e321e797a5a1dc58d
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 10:18:44 -0300

    Idade em Dias

---
+++ DesafiosJava/URI1020.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class URI1020 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int dias = entrada.nextInt();
+        int ano = dias/365;
+        dias = dias - (ano*365);
+        int mes = dias/30;
+        dias =  dias - (mes*30);
+        int dia = dias;
+
+        System.out.printf("%d ano(s)\n", ano);
+        System.out.printf("%d mes(es)\n", mes);
+        System.out.printf("%d dia(s)\n", dia);
+
+        entrada.close();
+    }
+}
