c8b506d8668b1abb69ff89995a7d071e2c418cc1
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 12:14:38 -0300

    Validador de Senhas

---
+++ DesafiosPython/URI2253.py
@@ -0,0 +1,27 @@
+while True:
+    try:
+        senha = input()
+        caracteres = len(senha)
+        minuscula = False
+        maiuscula = False
+        numero = False
+        if caracteres > 32 or caracteres < 6:
+            print('Senha invalida.')
+        else:
+            for i in range(caracteres):
+                if ord(senha[i]) > 64 and ord(senha[i]) < 91:
+                    maiuscula = True
+                elif ord(senha[i]) > 96 and ord(senha[i]) < 123:
+                    minuscula = True
+                elif ord(senha[i]) > 47 and ord(senha[i]) < 58:
+                    numero = True
+                else:
+                    maiucula = False
+                    break
+
+            if(maiuscula and minuscula and numero):
+                print('Senha valida.')
+            else:
+                print('Senha invalida.')
+    except EOFError:
+        break
