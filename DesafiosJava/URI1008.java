3c5f98e745131d91825846ab3f036c121a385db7
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 10:15:29 -0300

    Salário

---
+++ DesafiosJava/URI1008.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class URI1008 {
+    public static void main(String[] args) {
+        int n_funcionario, horas;
+        float q_horas;
+        Scanner entrada = new Scanner(System.in);
+        n_funcionario = entrada.nextInt();
+        horas = entrada.nextInt();
+        q_horas = entrada.nextFloat();
+        System.out.println("NUMBER = "+ n_funcionario);
+        System.out.printf("SALARY = U$ %.2f\n",horas*q_horas);
+        entrada.close();
+    }
+}
