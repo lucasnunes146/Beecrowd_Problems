73d722e76e85c9429ed4ebf4feeb5ec38c985425
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 10:14:32 -0300

    Produto Simples

---
+++ DesafiosJava/URI1004.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class URI1004 { // Beecrowd - Altere o nome da Classe para Main
+    public static void main(String[] args) {
+        int a, b;
+        Scanner entrada = new Scanner(System.in);
+        a = entrada.nextInt();
+        b = entrada.nextInt();
+        System.out.println("PROD = "+ (a*b));
+        entrada.close();
+        
+    }
+}
