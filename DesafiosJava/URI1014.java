bdd35bcbb5ebcf8f0f27394a209c284148fb1e35
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 10:17:25 -0300

    Consumo

---
+++ DesafiosJava/URI1014.java
@@ -0,0 +1,14 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class URI1014 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int x = entrada.nextInt();
+        double y = entrada.nextDouble();
+        double soma = x/y;
+        DecimalFormat decimalFormat = new DecimalFormat("#,###0.000");
+        System.out.println(decimalFormat.format(soma)+ " km/l");
+        entrada.close();
+    }
+}
