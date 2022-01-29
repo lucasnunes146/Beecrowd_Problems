6ec5ffcce65ee19c2fd2b77d50f860da7caee262
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 10:17:52 -0300

    Distância

---
+++ DesafiosJava/URI1016.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class URI1016 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int km = entrada.nextInt();
+        int calculo = (60 * km) / 30;
+        System.out.println(calculo+" minutos");
+        entrada.close();
+    }
+}
