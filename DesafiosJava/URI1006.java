b1fc7302bda4959e5ae81c6ba988333c6e08ff07
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 10:15:05 -0300

    Média 2

---
+++ DesafiosJava/URI1006.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class URI1006 {
+    public static void main(String[] args) {
+        double a, b, c, media;
+        Scanner entrada = new Scanner(System.in);
+        a = entrada.nextDouble()*2;
+        b = entrada.nextDouble()*3;
+        c = entrada.nextDouble()*5;
+        media = (a+b+c) / 10;
+        System.out.printf("MEDIA = %.1f\n", media);
+        entrada.close();
+
+    }
+}
