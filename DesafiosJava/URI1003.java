82b8dde74bd1001348d260bd5e8bae490217af74
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 10:14:10 -0300

    Soma Simples

---
+++ DesafiosJava/URI1003.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class URI1003 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner entrada = new Scanner(System.in);
+        a = entrada.nextInt();
+        b = entrada.nextInt();
+        System.out.println("SOMA = "+ (a+b));
+        entrada.close();
+    }
+}
