43f6497388430480145d60cef61d1da4c84c667a
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 10:16:17 -0300

    Esfera

---
+++ DesafiosJava/URI1011.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class URI1011 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        Double volume, pi = 3.14159;
+        double raio = entrada.nextDouble();
+        volume = (4.0/3)*pi* Math.pow(raio, 3);
+        System.out.printf("VOLUME = %.3f\n", volume);
+        entrada.close();
+    }
+}
