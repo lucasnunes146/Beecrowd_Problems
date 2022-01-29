6dafa26365c033376874ad55bfbfba7b11099d5e
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 13:24:36 -0300

    Tipos de Triângulos

---
+++ DesafiosJava/URI1045.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class URI1045 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String[] valores = entrada.nextLine().split(" ");
+
+        float[] numeros = new float[3];
+
+        for (int i = 0; i < 3; i++){
+            numeros[i] = Float.parseFloat(valores[i]);
+        }
+
+        Arrays.sort(numeros);
+        entrada.close();
+
+        double a = numeros[2];
+        double b = numeros[1];
+        double c = numeros[0];
+
+        if (a >= (b + c)) {
+            System.out.print("NAO FORMA TRIANGULO\n");
+
+        } else if (a * a > ((b * b) + (c * c))) {
+            System.out.print("TRIANGULO OBTUSANGULO\n");
+        }
+        if (a * a == ((b * b) + (c * c))) {
+            System.out.print("TRIANGULO RETANGULO\n");
+        }
+
+        if (a * a < ((b * b) + (c * c))) {
+            System.out.print("TRIANGULO ACUTANGULO\n");
+        }
+        if ((a == c) && (a == c)) {
+            System.out.print("TRIANGULO EQUILATERO\n");
+        }
+        if (((a == b) && (a != c)) || ((a == c) && (a != b)) || ((b == c) && (b != a))) {
+            System.out.print("TRIANGULO ISOSCELES\n");
+        }
+    }
+}
