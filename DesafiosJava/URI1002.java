e5f46a291e91df5cccd1e290f0518db045f7b39e
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 10:13:38 -0300

    Área do Círculo

---
+++ DesafiosJava/URI1002.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class URI1002 {
+    public static void main(String[] args) {
+        double r, a, n = 3.14159;
+        Scanner input = new Scanner(System.in);
+        r = input.nextDouble();
+        a = n * Math.pow(r, 2);
+        System.out.printf("A=%.4f\n",a);
+        input.close();
+
+    }
+}
