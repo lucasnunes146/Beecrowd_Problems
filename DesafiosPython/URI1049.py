8e0556b2976943fbb02887351ee1970a281fe653
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 11:15:40 -0300

    Animal

---
+++ DesafiosPython/URI1049.py
@@ -0,0 +1,20 @@
+n1 = input()
+n2 = input()
+n3 = input()
+
+if n3 == 'carnivoro' and n2 == 'ave' and n1 == 'vertebrado':
+    print('aguia')
+elif n3 == 'onivoro' and n2 == 'ave' and n1 == 'vertebrado':
+    print('pomba')
+elif n3 == 'onivoro' and n2 == 'mamifero' and n1 == 'vertebrado':
+    print('homem')
+elif n3 == 'herbivoro' and n2 == 'mamifero' and n1 == 'vertebrado':
+    print('vaca')
+elif n3 == 'hematofago' and n2 == 'inseto' and n1 == 'invertebrado':
+    print('pulga')
+elif n3 == 'herbivoro' and n2 == 'inseto' and n1 == 'invertebrado':
+    print('lagarta')
+elif n3 == 'hematofago' and n2 == 'anelideo' and n1 == 'invertebrado':
+    print('sanguessuga')
+elif n3 == 'onivoro' and n2 == 'anelideo' and n1 == 'invertebrado':
+    print('minhoca')
