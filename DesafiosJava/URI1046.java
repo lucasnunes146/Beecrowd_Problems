77d6c8b988613239ddf727276e9d72a0888d2e4c
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-29 21:52:27 -0300

    Termpo de Jogo

---
+++ DesafiosJava/URI1046.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class URI1046 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String[] valores = entrada.nextLine().split(" ");
+        entrada.close();
+
+        int horaInicial = Integer.parseInt(valores[0]);
+        int horaFim = Integer.parseInt(valores[1]);
+        int duracaoJogo;
+        
+        if (horaFim <= horaInicial){
+            duracaoJogo = (24 - horaInicial) + horaFim;
+            
+        }
+        else{
+            duracaoJogo = horaFim - horaInicial;
+        }  
+        System.out.println("O JOGO DUROU " + duracaoJogo + " HORA(S)");
+    }
+}
