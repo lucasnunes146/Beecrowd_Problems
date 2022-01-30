7e6cc81f474dae779234a17b99427f9bdea45a99
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-01-30 14:16:57 -0300

    Animal

---
+++ DesafiosJava/URI1049.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class URI1049 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        String subfilo = entrada.nextLine();
+        String classe = entrada.nextLine();
+        String ordem = entrada.nextLine();
+
+        if (subfilo.equals("vertebrado") && classe.equals("ave")  && ordem.equals("carnivoro") ){
+            System.out.println("aguia");
+        }
+        else if (subfilo.equals("vertebrado") && classe.equals("ave") && ordem.equals("onivoro")){
+            System.out.println("pomba");
+        }
+        else if (subfilo.equals("vertebrado") && classe.equals("mamifero") && ordem.equals("onivoro")){
+            System.out.println("homem");
+        }
+        else if (subfilo.equals("vertebrado") && classe.equals("mamifero") && ordem.equals("herbivoro")){
+            System.out.println("vaca");
+        }
+        else if (subfilo.equals("invertebrado") && classe.equals("inseto") && ordem.equals("hematofago")){
+            System.out.println("pulga");
+        }
+        else if (subfilo.equals("invertebrado") && classe.equals("inseto") && ordem.equals("herbivoro")){
+            System.out.println("lagarta");
+        }
+        else if (subfilo.equals("invertebrado") && classe.equals("anelideo") && ordem.equals("hematofago")){
+            System.out.println("sanguessuga");
+        }
+        else if (subfilo.equals("invertebrado") && classe.equals("anelideo") && ordem.equals("onivoro")){
+            System.out.println("minhoca");
+        }
+        entrada.close();
+    }
+}
