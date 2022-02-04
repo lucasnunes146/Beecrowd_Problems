84e05f1a72d544c4aed6f8604865aaf358ada481
Lucas Nunes <91094949+lucasnunes146@users.noreply.github.com>
2022-02-03 23:27:49 -0300

    Flutuação do PIB

---
+++ DesafiosJava/URI3346.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class URI3346 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String[] valores = entrada.nextLine().split(" ");
+
+
+        double f1 = ((Double.parseDouble(valores[0]) / 100)+1);
+        double f2 = ((Double.parseDouble(valores[1]) / 100)+1);
+
+        double flutuacaoPib = ((f1 * f2) -1) * 100;
+
+        System.out.printf("%.6f\n", flutuacaoPib);
+        entrada.close();
+    }
+}
