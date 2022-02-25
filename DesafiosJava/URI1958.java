a4d060576420ad90a8139a0e084666d7badf1c4b
lucasnunes146 <lucasnunes146@gmail.com>
2022-02-25 17:38:16 -0300

    Notação Científica

---
+++ DesafiosJava/URI1958.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class URI1958 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Double valor = sc.nextDouble();
+        System.out.printf("%+.4E\n", valor);
+        sc.close();
+      }
+}
