d0f5aeba9e7a908f36fcd4c989f150c9c64eb118
lucasnunes146 <lucasnunes146@gmail.com>
2022-03-12 14:48:22 -0300

    Sequencia IJ 4

---
+++ DesafiosJava/URI1098.java
@@ -0,0 +1,16 @@
+public class URI1098 {
+    public static void main(String[] args) {
+        double i, j;
+        for (i = 0; i <= 2; i += 0.2) {
+            for (j = 1; j <= 3; j++) {
+                if (i == 0 || i == 1 || i >= 1.9) {
+                    System.out.println("I=" + Math.round(i) + " J=" + Math.round((i + (j % 10))));
+                } else {
+                    System.out.printf("I=%.1f", i);
+                    System.out.printf(" J=%.1f", (i + (j % 10)));
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
